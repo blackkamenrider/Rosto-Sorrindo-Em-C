3b84112699a6a2ee0462a89607a345ce86d5be50
hosias <hosias.souza@gmail.com>
2022-09-23 12:51:58 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int rosto(){
+    printf("                                            \n\n\n");
+
+
+    printf("                                               ******                   ******\n");
+    //printf(" \n");
+
+    printf("                                                  ***                    ***        \n");
+    printf("                                               ***   ***       **     ***   ***     \n");
+    printf("                                              ***  *  ***     ****   ***  *  ***    \n");
+    printf("                                                **   **     ********   **   **      \n");
+    printf("                                                 ** **      ********    ** **       \n");
+    printf("                                                             ******                 \n");
+    printf("                                                              ****                  \n");
+    printf("                                                                                    \n");
+    printf("                                                   **                     **        \n");
+    printf("                                                      **               **           \n");
+    printf("                                                     **  **         **   **             \n");
+    printf("                                                      **       **       **             \n");
+    printf("                                                       **              **          \n");
+    printf("                                                        **            **           \n");
+    printf("                                                         **          **            \n");
+    printf("                                                           **       **             \n");
+    printf("                                                              *****               \n\n\n\n");
+
+}
+
+
+
+int main()
+{
+while(true){
+    for (int i = 0; i <200;i++){
+       if(i< 50){
+         printf("                                            \n\n\n");
+
+
+    printf("                                               ******                   ******\n");
+    //printf(" \n");
+
+    printf("                                                  ***                    ***        \n");
+    printf("                                               ***   ***       **     ***   ***     \n");
+    printf("                                              ***  *  ***     ****   ***  *  ***    \n");
+    printf("                                                **   **     ********   **   **      \n");
+    printf("                                                 ** **      ********    ** **       \n");
+    printf("                                                             ******                 \n");
+    printf("                                                              ****                  \n");
+    printf("                                                                                    \n");
+    printf("                                                           ***********              \n");
+    printf("                                                              *****                 \n");
+    printf("                                                               ***                  \n");
+    printf("                                                                                    \n");
+
+       }
+       system("cls");
+
+
+       if(i> 50 && i<100){
+         printf("                                            \n\n\n");
+
+
+    printf("                                               ******                   ******\n");
+    //printf(" \n");
+
+    printf("                                                  ***                    ***        \n");
+    printf("                                               ***   ***       **     ***   ***     \n");
+    printf("                                              ***  *  ***     ****   ***  *  ***    \n");
+    printf("                                                **   **     ********   **   **      \n");
+    printf("                                                 ** **      ********    ** **       \n");
+    printf("                                                             ******                 \n");
+    printf("                                                              ****                  \n");
+    printf("                                                                                    \n");
+    //printf("                                                                                  \n");
+    printf("                                                      ** ** ** ** ** **   **           \n");
+    printf("                                                      **                  **             \n");
+    printf("                                                       **                **              \n");
+    printf("                                                        ** ** ** ** **  **               \n");
+    printf("                                                                                        \n");
+    printf("                                                                                        \n");
+    printf("                                                                                        \n");
+    printf("                                                                                   \n\n\n\n");
+
+          }
+
+          if(i> 100 && i<150){
+         printf("                                            \n\n\n");
+
+
+    printf("                                               ******                   ******\n");
+    //printf(" \n");
+
+    printf("                                                  ***                    ***        \n");
+    printf("                                               ***   ***       **     ***   ***     \n");
+    printf("                                              ***  *  ***     ****   ***  *  ***    \n");
+    printf("                                                **   **     ********   **   **      \n");
+    printf("                                                 ** **      ********    ** **       \n");
+    printf("                                                             ******                 \n");
+    printf("                                                              ****                  \n");
+    printf("                                                                                    \n");
+    printf("                                                   **                       **      \n");
+    printf("                                                    **     **         **   **       \n");
+    printf("                                                     **         **        **        \n");
+    printf("                                                       **               **          \n");
+    printf("                                                         **            **           \n");
+    printf("                                                           ** ** ** **              \n");
+    printf("                                                                                    \n");
+    printf("                                                                                    \n");
+    printf("                                                                                    \n");
+    printf("                                                                                   \n\n\n");
+
+          }
+
+          if(i> 150 && i<200){
+          printf("                                               ******                   ******\n");
+    //printf(" \n");
+
+    printf("                                                  ***                    ***        \n");
+    printf("                                               ***   ***       **     ***   ***     \n");
+    printf("                                              ***  *  ***     ****   ***  *  ***    \n");
+    printf("                                                **   **     ********   **   **      \n");
+    printf("                                                 ** **      ********    ** **       \n");
+    printf("                                                             ******                 \n");
+    printf("                                                              ****                  \n");
+    printf("                                                                                    \n");
+    printf("                                                   **                     **        \n");
+    printf("                                                      **               **           \n");
+    printf("                                                     **  **         **   **             \n");
+    printf("                                                      **       **       **             \n");
+    printf("                                                       **              **          \n");
+    printf("                                                        **            **           \n");
+    printf("                                                         **          **            \n");
+    printf("                                                           **       **             \n");
+    printf("                                                              *****               \n\n\n\n");
+
+          }
+
+
+
+
+    }
+}
+    //printf("Hello world!\n");
+    return 0;
+}
